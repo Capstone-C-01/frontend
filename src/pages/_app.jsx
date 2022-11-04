503c61358a6a3320b4102eb5e7d38b2fb987b4ce
aldoarya <arudo.saka@gmail.com>
2022-11-04 11:59:32 +0700

    chore: Added Head meta

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,18 @@
 import { Layout } from '@/components/Layout';
+import Head from 'next/head';
 import '../styles/globals.css';
 import '../styles/tailwind.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Sistem Kebun Otomatis | Capstone 2022</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
