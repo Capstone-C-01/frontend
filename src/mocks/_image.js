530e1635fc26f61a179c2f0a4aae1b42e7d3cb17
aldoarya <arudo.saka@gmail.com>
2022-11-12 20:19:03 +0700

    feat: added chart data

---
+++ src/mocks/_image.js
@@ -0,0 +1,26 @@
+export const imagesPlantGallery = [
+  {
+    src: 'https://images.unsplash.com/photo-1667487230916-05f14fdbec5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+    alt: 'Temp Image'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'Temp Image'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1661956603025-8310b2e3036d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+    alt: 'Temp Image'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1667471728450-c3a75d76ff82?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
+    alt: 'Temp Image'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80',
+    alt: 'Temp Image'
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1667339406161-c7374c35749c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1475&q=80',
+    alt: 'Temp Image'
+  }
+];
---
+++ src/mocks/_sensors.js
@@ -0,0 +1,16 @@
+import moment from 'moment';
+import { boolean } from './boolean';
+import { percent, ph, price } from './number';
+
+export const sensorsData = [...Array(20)].map((item, index) => ({
+  _id: '636a08914376141f08b39b53' + index,
+  user_id: 'user_id-dev',
+  device_id: 'device_id-dev',
+  lamp_status: boolean[index],
+  water_level: percent[index],
+  ph_data: ph[index],
+  tds_data: price[index] * 10,
+  createdAt: moment().add(index, 'minute').toISOString(),
+  updatedAt: '2022-11-08T07:43:13.510Z',
+  __v: 0
+}));
