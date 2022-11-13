b2b33b4690973b77120e7b5f63ea16e7132b78d3
aldoarya <arudo.saka@gmail.com>
2022-11-13 22:53:56 +0700

    feat: Handle pH, TDS, and Lamp Control

--- src/components/Button/Toggle.jsx
+++ src/components/Button/Toggle.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from '@/utils/classNames';
 
-const Toggle = ({ ...props }) => {
+const Toggle = ({ checked, onClick, ...props }) => {
   return (
-    <label for="default-toggle-size" class="inline-flex relative items-center my-2 cursor-pointer">
-        <input type="checkbox" value="" id="default-toggle-size" class="sr-only peer"/>
-        <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-200 dark:peer-focus:ring-sky-500 rounded-full peer dark:bg-gray-400 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-400 peer-checked:bg-sky-400"></div>
+    <label for="default-toggle-size" class="relative my-2 inline-flex cursor-pointer items-center">
+      <input
+        type="checkbox"
+        id="default-toggle-size"
+        class="peer sr-only"
+        checked={checked}
+        onClick={onClick}
+      />
+      <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-sky-400 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-200 dark:border-gray-400 dark:bg-gray-400 dark:peer-focus:ring-sky-500"></div>
     </label>
   );
 };
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,7 @@ import '../styles/globals.css';
 import '../styles/tailwind.css';
 import 'react-image-lightbox/style.css';
 import { AuthGuard } from 'src/guard';
+import { Toaster } from 'react-hot-toast';
 
 export default function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
@@ -15,7 +16,12 @@ export default function MyApp({ Component, pageProps }) {
         <title>Sistem Kebun Otomatis | Capstone 2022</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <AuthGuard>{getLayout(<Component {...pageProps} />)}</AuthGuard>
+      <AuthGuard>
+        <main>
+          <Toaster />
+          {getLayout(<Component {...pageProps} />)}
+        </main>
+      </AuthGuard>
     </>
   );
 }
